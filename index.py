ffb051169535abe4fb1d734843dee65392b39c14
Sophie <sophie.cooper@7digital.com>
2020-04-30 12:03:37 +0100

    Refactor checkout function
     - Remove duplication
     - Extract discount calculation

--- index.py
+++ index.py
@@ -5,24 +5,25 @@ _items = {
     'D': 15
     }
 
+def _calculate_discount(quantity, quantity_needed, discount):
+  return (quantity // quantity_needed) * discount
+
 def checkout(items):
+  item_counts = {}
+
+  def item_count(item):
+    return item_counts.get(item, 0)
+
   total = 0
-  a_count = 0
-  b_count = 0
 
   for item in items:
-    if item == 'A':
-      a_count += 1
-
-    if item == 'B':
-      b_count += 1
+    item_counts[item] = item_count(item) + 1
 
     total += _items[item]
 
-  a_discount = (a_count // 3) * 20
-  total -= a_discount
+  a_discount = _calculate_discount(item_count('A'), 3, 20)
+  b_discount = _calculate_discount(item_count('B'), 2, 15)
 
-  b_discount = (b_count // 2) * 15
-  total -= b_discount
+  total -= a_discount + b_discount
 
   return total
